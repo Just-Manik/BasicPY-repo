ac57803e19e2fd70be511bb9d39d09e864cae9e0
Kuma-Manik <ursfren.manik@gmail.com>
2025-06-29 19:50:56 +0545

    basicc

---
+++ calculatorofusingbasicoperators.py
@@ -0,0 +1,20 @@
+#Q1 simply create a calculator that performs basic arithmetic operations
+print(2+3)  # Output: 5
+print(2-3)  # Output: -1    
+print(2*3)  # Output: 6
+print(2/3)  # Output: 0.6666666666666666 (float division)
+print(2//3)  # Output: 0 (floor division)
+print(21%3)  # Output: 0 (modulus, remainder of division)
+print(2**3)  # Output: 8 (exponentiation, 2 raised to the power of 3)
+
+print("ENter first number")
+a = int(input())
+print("Enter second number")
+b = int(input())
+print("Addition:", a + b)  # Output: Sum of a and b
+print("Subtraction:", a - b)  # Output: Difference of a and b       
+print("Multiplication:", a * b)  # Output: Product of a and b
+print("Division:", a / b)  # Output: Quotient of a and b
+print("Floor Division:", a // b)  # Output: Floor division of a by b
+print("Modulus:", a % b)  # Output: Remainder of a divided by b
+print("Exponentiation:", a ** b)  # Output: a raised to the power of b
